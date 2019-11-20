0f57834b1aafb41ad5825b5e5c36d494d841aaaa
Brett208 <15183337+Brett208@users.noreply.github.com>
2019-11-20 17:42:33 -0500

    Force unit array into specific memory address

--- DllMain.cpp
+++ DllMain.cpp
@@ -1,6 +1,31 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <HFL/Source/HFL.h>
+#include <cstdint>
+#include <cstddef>
+
+
+void PatchUnitArrayAddress()
+{
+	constexpr std::uintptr_t unitArrayPointerAddress = 0x54F848;
+	auto unitArrayPointer = reinterpret_cast<std::uintptr_t*>(unitArrayPointerAddress);
+	*unitArrayPointer += -(*unitArrayPointer) % 120;
+}
+
+//void PatchUnitArrayAddress()
+//{
+//	std::uintptr_t unitArray = 0x04170030;
+//	constexpr std::size_t unitArraySize = 0x3c078;
+//
+//	LPVOID result = VirtualAlloc(reinterpret_cast<LPVOID>(unitArray), unitArraySize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+//	while (result == NULL) {
+//		unitArray += 0x04170030;
+//		result = VirtualAlloc(reinterpret_cast<LPVOID>(unitArray), unitArraySize, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+//	}
+//
+//	constexpr std::uintptr_t unitArrayPointer = 0x54F848;
+//	*reinterpret_cast<std::uintptr_t*>(unitArrayPointer) = unitArray;
+//}
 
 BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
@@ -8,7 +33,7 @@ BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 	{
 		DisableThreadLibraryCalls(hinstDLL);
 		HFLInit();
-		DisableThreadLibraryCalls(hinstDLL);
+		PatchUnitArrayAddress();
 	}
 	else if (fdwReason == DLL_PROCESS_DETACH)
 	{
